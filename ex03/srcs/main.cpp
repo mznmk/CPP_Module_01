2800a6348c9a619d38cb5fd1e606e1578cd6ddf6
mznmk <mika_develop@mznmk.com>
2022-04-12 12:09:07 +0900

    [ex03] update ex03

--- ex03/srcs/main.cpp
+++ ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmizuno <mmizuno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 14:55:00 by mmizuno           #+#    #+#             */
-/*   Updated: 2022/04/12 11:39:02 by mmizuno          ###   ########.fr       */
+/*   Updated: 2022/04/12 12:08:24 by mmizuno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,54 @@
 #include "../incs/HumanA.hpp"
 #include "../incs/HumanB.hpp"
 
-// [ normal case ]
 int main()
 {
-    // [ HumanA ]
-    // crude spiked club" -> "some other type of club"
-    {
-        Weapon club = Weapon("crude spiked club");
-        HumanA bob("Bob", club);
-        bob.attack();
-        club.setType("some other type of club");
-        bob.attack();
-    }
-    // [ HumanB ]
-    {
-        Weapon club = Weapon("crude spiked club");
-        HumanB jim("Jim");
-        jim.setWeapon(club);
-        jim.attack();
-        club.setType("some other type of club");
-        jim.attack();
-    }
-    return 0;
-}
+    // [ normal case ]
+        std::cout << "[ normal case ]" << std::endl;
 
-// // [ corner case ]
-// int main()
-// {
-//     // [ HumanA ]
-//     // "crude spiked club" -> "some other type of club"
-//     {
-//         Weapon club = Weapon("crude spiked club");
-//         HumanA bob("Bob", club);
-//         bob.attack();
-//         club.setType("some other type of club");
-//         bob.attack();
-//     }
-//     // [ HumanB ]
-//     // "hand... (T T)" -> "some other type of club"
-//     {
-//         Weapon club = Weapon("crude spiked club");
-//         HumanB jim("Jim");
-//         jim.attack();           // <--- swap
-//         jim.setWeapon(club);    // <-/ 
-//         club.setType("some other type of club");
-//         jim.attack();
-//     }
-//     return 0;
-// }
+        // < HumanA >
+        std::cout << "< HumanA: Bob >" << std::endl;
+        {
+            Weapon club = Weapon("crude spiked club");
+            HumanA bob("Bob", club);
+            bob.attack();
+            club.setType("some other type of club");
+            bob.attack();
+        }
+        // < HumanB >
+        std::cout << "< HumanB: Jim >" << std::endl;
+        {
+            Weapon club = Weapon("crude spiked club");
+            HumanB jim("Jim");
+            jim.setWeapon(club);
+            jim.attack();
+            club.setType("some other type of club");
+            jim.attack();
+        }
+
+    // [ corner case ]
+        std::cout << "[ corner case ]" << std::endl;
+
+        // < HumanA >
+        std::cout << "< HumanA: Bob >" << std::endl;
+        {
+            Weapon club = Weapon("crude spiked club");
+            HumanA bob("Bob", club);
+            bob.attack();
+            club.setType("some other type of club");
+            bob.attack();
+        }
+        // < HumanB >
+        std::cout << "< HumanB: Jim >" << std::endl;
+        {
+            Weapon club = Weapon("crude spiked club");
+            HumanB jim("Jim");
+            jim.attack();           // <--- swap
+            jim.setWeapon(club);    // <-/ 
+            club.setType("some other type of club");
+            jim.attack();
+        }
+
+    // [ return ]
+    return (0);
+}
